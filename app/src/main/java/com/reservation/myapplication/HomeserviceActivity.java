68dbce4a8ceedc7059e235156376d658982bf45f
SuwonLim <limsoft0401@kakao.com>
2021-08-19 04:49:42 +0900

    방문포장 서비스 _ gridLayout 메뉴구성, 추가, 리스트

--- app/src/main/java/com/reservation/myapplication/HomeserviceActivity.java
+++ app/src/main/java/com/reservation/myapplication/HomeserviceActivity.java
@@ -1,6 +1,8 @@
 package com.reservation.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -22,6 +24,22 @@ implements ListFragment.ImageSelectionCallback{
         listFragment = (ListFragment) fragmentManager.findFragmentById(R.id.listFragment);
         viewerFragment = (ViewerFragment) fragmentManager.findFragmentById(R.id.viewerFragment);
 
+        Button button_add = findViewById(R.id.home_menuAdd);
+        Button button_orderList = findViewById(R.id.home_orderList);
+
+        button_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        button_orderList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 
