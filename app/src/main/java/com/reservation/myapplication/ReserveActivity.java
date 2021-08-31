4781c9856212f30d241a73f6b78cc248d0ba0da7
SuwonLim <limsoft0401@kakao.com>
2021-08-31 13:41:29 +0900

    reservation function_ sending reservation complete

--- app/src/main/java/com/reservation/myapplication/ReserveActivity.java
+++ app/src/main/java/com/reservation/myapplication/ReserveActivity.java
@@ -1,7 +1,9 @@
 package com.reservation.myapplication;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -13,21 +15,48 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class ReserveActivity extends AppCompatActivity {
     ConstraintLayout reserveActivityLayout;
     EditText askEditText;
     EditText customerName;
     Spinner reserve_people;
+    TextView reserve_final;
+    String date;
+    String final_reservation;
+    int reserve_number;
+
+    Calendar myCalendar = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener myDatePicker = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH,month);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            String myFormat = "yyyy/MM/dd";
+            SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.KOREA);
+            date = dateFormat.format(myCalendar.getTime());
+            final_reservation = date;
+            reserve_final.setText(final_reservation);
+        }
+    };
 
     public void exit(){
         super.onBackPressed();
@@ -63,14 +92,19 @@ public class ReserveActivity extends AppCompatActivity {
         askEditText = (EditText) findViewById(R.id.edittext_Ask);
         customerName = (EditText) findViewById(R.id.reserve_Customer);
 
+
         reserveActivityLayout = (ConstraintLayout) findViewById(R.id.layout2);
-        Button enter_button = (Button) findViewById(R.id.enter_button);
+        reserve_people = (Spinner) findViewById(R.id.reserve_spinner);
+        reserve_final = (TextView) findViewById(R.id.reserve_finalView);
+
         CheckBox checkBox_people = (CheckBox) findViewById(R.id.checkbox1);
         CheckBox checkBox_corona = (CheckBox) findViewById(R.id.checkbox2);
         ConstraintLayout layout_reserve = (ConstraintLayout) findViewById(R.id.layout2);
-        reserve_people = (Spinner) findViewById(R.id.reserve_spinner);
         Button homeserviceButton = (Button) findViewById(R.id.home_service_button);
-
+        Button enter_button = (Button) findViewById(R.id.enter_button);
+        Button date_button = (Button) findViewById(R.id.date_button);
+        Button time_button = (Button) findViewById(R.id.time_button);
+        // 인원 스피너 기본값 선택
         reserve_people.setSelection(0);
         //String howManyPerson = reserve_people.getSelectedItem().toString();
 
@@ -78,19 +112,53 @@ public class ReserveActivity extends AppCompatActivity {
         ArrayAdapter peopleAdapter = ArrayAdapter.createFromResource(this,R.array.reserve_people, android.R.layout.simple_spinner_dropdown_item);
         reserve_people.setAdapter(peopleAdapter);
 
+        // 예약 확정 버튼
         enter_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 enterPopup();
+            }
+        });
+
+        // 날짜 선택 버튼
+        date_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(ReserveActivity.this, myDatePicker, myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH) ).show();
 
 
             }
         });
 
+        // 시간 선택 버튼
+        time_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mtimePicker;
+                mtimePicker = new TimePickerDialog(ReserveActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
+                        if(selectedHour > 20){
+                            Toast.makeText(getApplicationContext(), "불가능한 시간입니다 다시 선택해주세요", Toast.LENGTH_LONG);
+                        }
+                        final_reservation += " "+ selectedHour +"시 "+selectedMinute+"분 예약입니다";
+                        reserve_final.setText(final_reservation);
+                    }
+                }, hour, minute, true);
 
+                mtimePicker.setTitle("예약 시간을 지정합니다");
+                mtimePicker.show();
+
+            }
+        });
 
     }
+
+
 
 
     void enterPopup(){
@@ -103,7 +171,8 @@ public class ReserveActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 sendingMessage();
                 Information person1 = new Information();
-                Toast.makeText(getApplicationContext(), "귀하의 예약번호는 \n"+ person1.getReserveNum()+" 입니다",Toast.LENGTH_LONG).show();
+                reserve_number = person1.getReserveNum();
+                Toast.makeText(getApplicationContext(), "귀하의 예약번호는 \n"+ reserve_number+" 입니다",Toast.LENGTH_LONG).show();
             }
         });
 
@@ -127,9 +196,11 @@ public class ReserveActivity extends AppCompatActivity {
         String askedMessage = askEditText.getText().toString();
         String peopleNum = reserve_people.getSelectedItem().toString();
         String message_content = "예약이 도착했습니다"
+                +"\n예약번호: "+ reserve_number
                 +"\n이름: " +customerName.getText().toString()+
                 "\n인원수: "+peopleNum
-                +"\n***요청사항***\n" + askedMessage;
+                +"\n예약일시: "+final_reservation
+                +"\n\n***요청사항***\n" + askedMessage;
 
         ArrayList<String> arrayList = new ArrayList<>();
         arrayList = smsManager.divideMessage(message_content);
@@ -148,7 +219,7 @@ public class ReserveActivity extends AppCompatActivity {
                 switch (getResultCode()){
                     case Activity.RESULT_OK:
                         // 전송 성공
-                        Toast.makeText(getApplicationContext(), "전송 완료", Toast.LENGTH_SHORT).show();
+                        makeSnackbar(ReserveActivity.this, reserveActivityLayout, "예약문자가 전송되었습니다. 답장이 와야 예약이 확정됩니다. \n예약번호 "+reserve_number, Snackbar.LENGTH_LONG);
                         break;
                     case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
                         // 전송 실패
@@ -177,7 +248,7 @@ public class ReserveActivity extends AppCompatActivity {
                 switch (getResultCode()){
                     case Activity.RESULT_OK:
                         // 도착 완료
-                        Toast.makeText(getApplicationContext(), "SMS 도착 완료", Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(getApplicationContext(), "SMS 도착 완료", Toast.LENGTH_SHORT).show();
                         break;
                     case Activity.RESULT_CANCELED:
                         // 도착 안됨
@@ -197,6 +268,10 @@ public class ReserveActivity extends AppCompatActivity {
 
     }
 
+    void makeSnackbar(Context context, View view ,String message, int durationTime){
+        Snackbar.make(context, view, message, durationTime).show();
+
+    }
 
 }
 
